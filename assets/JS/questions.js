46da67af8d26517c176dd17ec20314939de88e78
Alex Papadakis <Lx.papad@gmail.com>
2023-12-09 13:26:55 +0200

    Adds the questions to be used in the quiz

---
+++ assets/JS/questions.js
@@ -0,0 +1,63 @@
+var questions = [
+    {
+        question: "JavaScript is an __________ language.",
+        choices: ["Object-Oriented", "Object-Based", "Procedural", "Markdown"],
+        correct: 0
+    },
+
+    {
+        question: "Which is used to define a variable in JavaScript?",
+        choices: ["add", "var", "since", "None of the above"],
+        correct: 1
+    },
+
+    {
+        question: "Which of the following methods is used to access HTML elements using Javascript?",
+        choices: ["getElementbyId()", "getElementsByClassName()", "Both of the above", "None of the above"],
+        correct: 2
+    },
+
+    {
+        question: "What keyword is used to check whether a given property is valid or not?",
+        choices: ["in", "is in", "exists", "lies"],
+        correct: 0
+    },
+
+    {
+        question: "Which function is used to serialize an object into a JSON string in Javascript?",
+        choices: ["parse()", "convert()", "serialize()", "stringify()"],
+        correct: 3
+    },
+
+    {
+        question: "Which of the following are closures in Javascript?",
+        choices: ["Variables", "Functions", "Objects", "All of the above"],
+        correct: 3
+    },
+
+    {
+        question: "Which of the following is NOT a Javascript framework?",
+        choices: ["React", "Bootstrap", "Vue", "Node"],
+        correct: 1
+    },
+
+    {
+        question: "What keyword is used to declare an asynchronous function in Javascript?",
+        choices: ["async", "nosync", "desync", "syncless"],
+        correct: 0
+    },
+
+    {
+        question: "How do we write a comment in javascript?",
+        choices: ["/* */", "//", "#", "<!-- -->"],
+        correct: 1
+    },
+
+    {
+        question: "Which of the following are NOT server-side JavaScript objects?",
+        choices: ["Date", "FileUpload", "Function", "All of the above"],
+        correct: 3
+    },
+]
+
+// Questions taken from or inspired by the quiz at https://www.interviewbit.com/javascript-mcq/
